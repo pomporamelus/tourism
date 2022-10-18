eef61daa393a5b1435d4f0dde8122d4d2ec2eaa0
unknown <emir.zhakypov@gmail.com>
2022-10-18 20:13:37 +0600

    changed main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  const config = new DocumentBuilder().addBearerAuth()
-   .setTitle('tourism documentation')
-   .build()
-   const document = SwaggerModule.createDocument(app,config)
-   await SwaggerModule.setup('api',app,document)
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('E-SHOP')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.PORT;
+  await app.listen(PORT || 3000, () => {
+    console.log(`Server started on PORT ${PORT}`);
+  })
+
 }
 bootstrap();
+// git add .
+// git commit -m ""
+// git push heroku <branch>
